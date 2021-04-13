f4dbce2fb9e3643a087f1f5c0b53d6bf54c7f22f
Denyse220 <denysedufitimana@gmail.com>
2021-04-13 10:25:01 +0200

    my codes to be pushed

---
+++ js/index.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  //user Interface
+  $("#formFull").submit(function(event){
+      
+    function sizePizza() {
+      var p_Size = document.getElementById("sizes").value;
+      return parseInt(p_Size);
+    }
+    function crustPizza() {
+      var p_Crust = document.getElementById("crusts").value;
+      return parseInt(p_Crust);
+    }
+    function toppingPizza() {
+      var p_Topping = document.getElementById("toppings").value;
+      return parseInt(p_Topping);
+    }
+    function numberPizza() {
+      var p_Number = document.getElementById("inputNumber").value;
+      return parseInt(p_Number);
+    }
+    function getPizza(sizePizza, crustPizza, toppingPizza, quantity) {
+      this.mySize = sizePizza;
+      this.myCrust = crustPizza;
+      this.myToppings = toppingPizza;
+      this.myNumber = quantity;
+    }
+    //business Logic(a constructor)
+    var selectOption = new getPizza(sizePizza(), crustPizza(), toppingPizza(), numberPizza());
+    var totalCost =
+      (selectOption.mySize +
+        selectOption.myCrust+
+        selectOption.myToppings) *
+        selectOption.myNumber;
+    //user interface to get results
+        
+        $(".results").show();
+        document.getElementById("paraSize").innerHTML=selectOption.mySize + (" Rwf");
+        document.getElementById("paraCrust").innerHTML= selectOption.myCrust + (" Rwf");
+        document.getElementById("paraToppings").innerHTML=selectOption.myToppings + (" Rwf");
+        document.getElementById("answer").innerHTML=totalCost+(" Rwf");
+   
+    event.preventDefault();
+  });
+});
+$(document).ready(function(){
+  $(".DeliverNotes").click(function(){
+    $("#formD").show();
+  });
+});
+$(document).ready(function(){
+  $("form#formD").submit(function(event){
+      var Name = $("input#Name").val();
+      var Phone = $("input#Phone").val();
+      var Location = $("input#Location").val();
+      alert("Dear " + Name + " Who is located at " + Location + " we will get back to you on " + Phone + " Whenever the Pizza are delivered to you");
+      event.preventDefault();
+
+  });
+
+});
+$(document).ready(function(){
+  $("#contform").submit(function(){
+      var inputName = $("input#inputName").val();
+      alert(inputName + " we have received your message. Thank you for reaching out to us.");
+
+  });
+
+});
